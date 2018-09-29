bbcf64e7d474f14e67e455b11fb102d5e80a9239
Piotrowski Radosław <radek.piotrowski@gmail.com>
2018-09-29 14:23:18 +0200

    zadanie nr2 czytaj więcej

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     
     var menuButton = document.querySelector('.for-dropdown');
     var subMenu = document.querySelector('.dropdown');
-    
+        
     menuButton.addEventListener('mouseover', function () {
         //console.log('hej');
         subMenu.style.display = 'block';
@@ -16,6 +16,34 @@ document.addEventListener('DOMContentLoaded', function () {
         //console.log('hej');
         subMenu.style.display = 'none';
     });
+    
+    var buttons = document.querySelectorAll('.read-more');
+    
+    
+    function showHide() {
+        var textArea = this.previousElementSibling;
+
+      // Poniższa linijka sprawdza, czy `style.display` jest ustawiony na `none` LUB jest pusty
+      // Dwie kreski obok siebie, czyli || oznaczają LUB, więc aby wykonał się kod podany w nawiasie klamrowym
+      // warunek po jednej albo po drugiej stronie kresek musi być spełniony
+
+      if (textArea.style.display === 'none' || textArea.style.display === '') {
+
+        // ten kod wykona się, gdy spełni się jeden z powyższych warunków
+        textArea.style.display = 'block';
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      } else {
+
+        // ten kod zadziała, gdy warunek podany w `if` nie będzie spełniony, czyli w przypadku
+        // gdy `style.display` jest inny niż `none` oraz gdy nie jest pusty
+        textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+      }        
+    };
+    //buttons[0].addEventListener('click', showHide);
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide);
+    }
     
     
 });
